4f7a87ff3a56070e6bc801eda7212f7027a797c8
MMDPROJECT <mmdvioleti@gmail.com>
2024-04-09 01:03:48 +0330

    commented sample codes & functions &  added on i as the maximum number of iterations

--- bisection.py
+++ bisection.py
@@ -3,35 +3,35 @@
 import numpy as np
 
 # for example function f(x) is given
-def f(x):
-    return np.cos(x)
+# def f(x):
+#     return np.cos(x)
 
 # we start off by finding a root interval; It will return a single number if it has already found the root or return a tuple consisting (a, b)
-def root_interval_finder(f):
-    a, b = 1, 2
-    f_a, f_b = f(a), f(b)
+# def root_interval_finder(f):
+#     a, b = 1, 2
+#     f_a, f_b = f(a), f(b)
 
-    while True:
-        if f_a == 0: return a
-        elif f_b == 0: return b
-        elif f_a * f_b < 0: return (a, b)
-        else: 
-            a -= 10
-            b += 10
+#     while True:
+#         if f_a == 0: return a
+#         elif f_b == 0: return b
+#         elif f_a * f_b < 0: return (a, b)
+#         else: 
+#             a -= 10
+#             b += 10
 
 # then we start by diving the length of the interval to get to the root
-# f: function that we want to find the root of, a: start of the interval, b: end of the interval, e: maximum desired error, d: number of digits that we are allowed to store after decimal point
-def bisection_method(f, a, b, e, d):
+# f: function that we want to find the root of, a: start of the interval, b: end of the interval, e: maximum desired error, d: number of digits that we are allowed to store after decimal point, i: maximum number of iteration desired, which is 20 by default
+def bisection_method(f, a, b, e, d, i = 20):
     a, b = round(a, d), round(b, d)
     iterations = list() # each element will be [a: start point of the interval, f_a, b: end point of the interval, f_b, c: middle of the interval, f_c, i: which iteration it is]
-    i = 0
-    while True:
-        i += 1
+    iteration_number = 0
+    while iteration_number <= i:
+        iteration_number += 1
         c = round((a + b) / 2, d)
         f_a, f_b, f_c = round(f(a), d), round(f(b), d), round(f(c), d)
 
         # storing information
-        iterations.append([a, f_a, b, f_b, c, f_c, i])
+        iterations.append([a, f_a, b, f_b, c, f_c, iteration_number])
 
         # checking if c is the root
         if f_c == 0: return [c, iterations]
@@ -42,7 +42,3 @@ def bisection_method(f, a, b, e, d):
         # checking if the error is low
         if f_c <= e or abs(a - b) <= e: 
             return [c, iterations]
-
-
-print(root_interval_finder(f))
-print(bisection_method(f, *root_interval_finder(f), 10**(-50), 50))
