ef4026cfbeaf7cd4924ff70f5a0ca92074ff1919
MMDPROJECT <mmdvioleti@gmail.com>
2024-05-19 16:35:35 +0330

    added evaluation at a specific point

--- InterpolationMethods/ChebyshewMethod/chebyshew.py
+++ InterpolationMethods/ChebyshewMethod/chebyshew.py
@@ -48,10 +48,16 @@ f = sympy.utilities.lambdify("x", str_of_function, "numpy")
 n = int(input("Enter the degree of the polynomial that you want to interpolate the function with: "))
 chebyshew_nodes = get_chebyshew_roots(n) # here we calculate the chebyshew nodes for the n-th degree interpolation
 start, end = map(float, input("Enter the start and end intervals that you want to plot the function and it's interpolation: ").split())
+inp = input("Is there any specific point that you want to be calculated?")
 divided_differences = get_divided_differences(f, chebyshew_nodes, start, end) # here we calculate divided differences using chebyshew nodes and the function f
 xs = np.linspace(start, end, 100) # we divide the interval [start, end] into 100 nodes
 Y = f(xs) # Real values of the function at the specified points xs
 Y_hat = P_n(chebyshew_nodes, divided_differences, xs, start, end) # Estimation of the function using interpolation
+if inp != '': 
+    global single_x_input
+    single_x_input = float(inp)
+    print(f"Value of interpolation function at {single_x_input} : {P_n(chebyshew_nodes, divided_differences, single_x_input, start, end)}")
+    print(f"Value of original function at {single_x_input} : {f(single_x_input)}")
 plt.plot(xs, Y, color = 'red', markersize = "1", linewidth = "2", linestyle = "dashdot", marker = "*", label = "f(x)")
 plt.plot(xs, Y_hat, color = "blue", markersize = "0.25", linewidth = "0.5", linestyle = "-", marker = "o", label = "Pn(x)")
 plt.legend()
