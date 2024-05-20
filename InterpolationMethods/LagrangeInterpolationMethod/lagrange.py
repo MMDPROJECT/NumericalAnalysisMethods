b3a84ca4bef783b8dd6ffd9345f60bffd0838a4b
Mobina Kargar <119706098+Grmamobin@users.noreply.github.com>
2024-05-20 20:20:45 +0330

    remove MMD code from my code 😒 + task completed

--- InterpolationMethods/LagrangeInterpolationMethod/lagrange.py
+++ InterpolationMethods/LagrangeInterpolationMethod/lagrange.py
@@ -2,68 +2,90 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sympy
 import sympy.utilities
+from sympy import *
 
-def L_i(x, xs, i):
-    n = len(xs)
-    product = 1
-    for j in range(n):
-        if j != i:
-            product *= (x - xs[j])/(xs[i] - xs[j])
-    return product
+x = []
+y = []
+index = 0
 
-def P_n(x, xs, ys):
-    n = len(xs)
-    print(n)
-    summation = 0
-    for i in range(n):
-        # print(f"i :" + i)
-        summation += L_i(x, xs, i) * ys[i]
-    return summation
-
-
-xs = []
-ys = []
-x_sample = None
-y_sample = None
-n = 0
-
-f = input("Enter the function or press enter if you just want to enter data points: ")
-if f == "":
-    while(1):
-        point = input(f"x{n}: ")
-        if point != "":
-            point = list(map(float, point.split()))
-            xs.append(point[0])
-            ys.append(point[1])
-        else:
+# Convert (x,y) to their determination list
+fx = input("Enter the function or press enter if you just want to enter data points: ")
+if fx == "":
+    print()
+    print("********************************")
+    print("Add point like this format (x,y)")
+    print("********************************")
+    print()
+    while True:
+        data = input("(x" + str(index) + ", y" + str(index) + "):: ")
+        index += 1
+        data = data.strip("()").replace(" ", "")
+        point = list(map(float, data.split(",")))
+        continue_point = input("Do you still want to add points? (y/n) ")
+        x.append(point[0])
+        y.append(point[1])
+        if continue_point == "n":
             break
-        n += 1
-       
-    x_sample = np.linspace(min(xs), max(xs), 100)
-    y_sample = P_n(x_sample, xs, ys)
-    xs = np.array(xs)
-    ys = np.array(ys)
-    plt.plot(x_sample, y_sample, label = "Interpolation over data points")
-
-
 else:
-    f = sympy.utilities.lambdify("x", f, "numpy")   
-    while(1):
-        point = input(f"x{n}: ")
-        if point != "":
-            point = list(map(float, point.split()))
-            xs.append(point[0])
-            ys.append(f(point[0]))
-        else:
+    fx = sympy.utilities.lambdify("x", fx, "numpy")
+    while True:
+        data = input("x" + str(index) + ":: ")
+        index += 1
+        point = list(map(float, data.split()))
+        continue_point = input("Do you still want to add points? (y/n) ")
+        x.append(point[0])
+        y.append(fx(point[0]))
+        if continue_point == "n":
             break
-        n += 1
-    x_sample = np.linspace(min(xs), max(xs), 100)
-    y_real = f(x_sample)
-    y_sample = P_n(x_sample, xs, ys)
-    xs = np.array(xs)
-    ys = np.array(ys)
-    plt.plot(x_sample, y_sample, label = "Interpolation over data points")
-    plt.plot(x_sample, y_real, label = "Original function")
-    
-plt.legend()
+print()
+n = int(input("Enter the degree n (for Pn(x)):: "))
+print()
+chosen_x = symbols("x")
+
+
+# Total Formulation
+def f(i):
+    return y[i]
+
+
+def L(i, x):
+    mult = 1
+    for j in range(len(x)):
+        if i != j:
+            mult *= (chosen_x - x[j]) / (x[i] - x[j])
+    return mult
+
+
+def pn(x, f, n):
+    sums = 0
+    for i in range(n + 1):
+        sums += f(i) * L(i, x)
+    return sums
+
+
+print("*** Pn(x) ***")
+print("p" + str(n) + "(x):: " + str(pn(x, f, n)))
+print()
+px = list(
+    map(
+        float,
+        input(
+            "Enter the x-values at which to find the interpolation (x for pn(x)): "
+        ).split(),
+    )
+)
+# PLOT
+p = pn(x, f, n)
+x_range = np.linspace(min(x), max(x), 100)
+y_range = [float(p.subs(chosen_x, x_val)) for x_val in x_range]
+
+plt.figure(figsize=(12, 6))
+plt.plot(x_range, y_range)
+plt.scatter(
+    px, [float(p.subs(chosen_x, px_val)) for px_val in px], color="g", marker="o"
+)
+plt.title(f"P{n}")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid()
 plt.show()
