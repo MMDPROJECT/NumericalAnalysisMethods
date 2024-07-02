2b723d06deb70b25b93cd6b477b66771365cb831
MMDPROJECT <mmdvioleti@gmail.com>
2024-07-02 17:11:33 +0330

    add comments - refactor code for lagrangemmd

--- InterpolationMethods/LagrangeInterpolationMethod/lagrangemmd.py
+++ InterpolationMethods/LagrangeInterpolationMethod/lagrangemmd.py
@@ -4,66 +4,89 @@ import sympy
 import sympy.utilities
 
 def L_i(x, xs, i):
+    """
+    Calculates the Lagrange basis polynomial Li(x).
+    
+    Parameters:
+    - x: Point at which to evaluate the basis polynomial.
+    - xs: Array of x-coordinates of the data points.
+    - i: Index of the current basis polynomial.
+    
+    Returns:
+    The value of the Lagrange basis polynomial Li(x).
+    """
     n = len(xs)
     product = 1
     for j in range(n):
-        if j != i:
-            product *= (x - xs[j])/(xs[i] - xs[j])
+        if j!= i:
+            product *= (x - xs[j]) / (xs[i] - xs[j])
     return product
 
 def P_n(x, xs, ys):
+    """
+    Evaluates the Lagrange polynomial P_n(x) at a given point x.
+    
+    Parameters:
+    - x: Point at which to evaluate the polynomial.
+    - xs: Array of x-coordinates of the data points.
+    - ys: Array of y-values corresponding to the data points.
+    
+    Returns:
+    The value of the Lagrange polynomial P_n(x).
+    """
     n = len(xs)
-    print(n)
     summation = 0
     for i in range(n):
-        # print(f"i :" + i)
         summation += L_i(x, xs, i) * ys[i]
     return summation
 
-
+# Initialize empty lists for x and y coordinates of data points
 xs = []
 ys = []
-x_sample = None
-y_sample = None
+
+# Initialize counter for data points
 n = 0
 
-f = input("Enter the function or press enter if you just want to enter data points: ")
-if f == "":
-    while(1):
+# Prompt user for input
+f_input = input("Enter the function or press enter if you just want to enter data points: ")
+
+if f_input == "":
+    # User enters data points
+    while True:
         point = input(f"x{n}: ")
-        if point != "":
-            point = list(map(float, point.split()))
-            xs.append(point[0])
-            ys.append(point[1])
-        else:
+        if not point:
             break
-        n += 1
-       
+        point = list(map(float, point.split()))
+        xs.append(point[0])
+        ys.append(point[1])
+        n += 1  # Increment counter for data points
+        
+    # Calculate interpolated values over the sampled x-range
     x_sample = np.linspace(min(xs), max(xs), 100)
     y_sample = P_n(x_sample, xs, ys)
     xs = np.array(xs)
     ys = np.array(ys)
-    plt.plot(x_sample, y_sample, label = "Interpolation over data points")
-
-
+    plt.plot(x_sample, y_sample, label="Interpolation over data points")
 else:
-    f = sympy.utilities.lambdify("x", f, "numpy")   
-    while(1):
+    # User defines a function
+    f_expression = sympy.utilities.lambdify("x", f_input, "numpy")
+    while True:
         point = input(f"x{n}: ")
-        if point != "":
-            point = list(map(float, point.split()))
-            xs.append(point[0])
-            ys.append(f(point[0]))
-        else:
+        if not point:
             break
-        n += 1
+        point = list(map(float, point.split()))
+        xs.append(point[0])
+        ys.append(f_expression(point[0]))
+        n += 1  # Increment counter for data points
+    
+    # Calculate interpolated values and plot
     x_sample = np.linspace(min(xs), max(xs), 100)
-    y_real = f(x_sample)
+    y_real = f_expression(x_sample)  # Original function values
     y_sample = P_n(x_sample, xs, ys)
     xs = np.array(xs)
     ys = np.array(ys)
-    plt.plot(x_sample, y_sample, label = "Interpolation over data points")
-    plt.plot(x_sample, y_real, label = "Original function")
-    
+    plt.plot(x_sample, y_sample, label="Interpolation over data points")
+    plt.plot(x_sample, y_real, label="Original function")
+
 plt.legend()
 plt.show()
