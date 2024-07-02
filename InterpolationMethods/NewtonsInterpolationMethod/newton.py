2baf9359fe325e78991365ac0fa675e19d8ff2f0
MMDPROJECT <mmdvioleti@gmail.com>
2024-07-02 17:19:21 +0330

    add comments - refactor code to newton

--- InterpolationMethods/NewtonsInterpolationMethod/newton.py
+++ InterpolationMethods/NewtonsInterpolationMethod/newton.py
@@ -1,28 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sympy.utilities.lambdify
-    
+import sympy
+
 def get_divided_difference(points, start_index, end_index):
-    if start_index == end_index: return points[start_index][1]
-    elif end_index == start_index + 1: 
-        return (points[end_index][1] - points[start_index][1])/(points[end_index][0] - points[start_index][0])
-    else: 
-        return (get_divided_difference(points, start_index + 1, end_index) - get_divided_difference(points, start_index, end_index - 1))/(points[end_index][0] - points[start_index][0])
-    
-def interpolation_with_points():    
-    # Reading the interpolation points
+    """
+    Calculates divided differences recursively.
+    """
+    if start_index == end_index:
+        return points[start_index][1]
+    elif end_index == start_index + 1:
+        return (points[end_index][1] - points[start_index][1]) / (points[end_index][0] - points[start_index][0])
+    else:
+        return (get_divided_difference(points, start_index + 1, end_index) -
+                get_divided_difference(points, start_index, end_index - 1)) / (points[end_index][0] - points[start_index][0])
+
+def interpolation_with_points():
+    """
+    Interpolates using user-provided points.
+    """
     n = 0
     points = []
-    while(1):
+    while True:
         point = input(f"x{n} f(x{n}): ")
-        if point != "":
-            point = list(map(float, point.split()))
-            points.append(point)
-        else:
+        if not point:
             break
+        point = list(map(float, point.split()))
+        points.append(point)
         n += 1
 
-    # finding the interpolation funciton
     x = sympy.Symbol('x')
     interpolation_function = 0
     for i in range(n):
@@ -37,37 +42,34 @@ def interpolation_with_points():
 
     f = sympy.utilities.lambdify("x", str(interpolation_function), "numpy")
 
-    # plotting points
-    fig = plt.subplot()
-    fig.scatter([i[0] for i in points], [i[1] for i in points])
+    fig, ax = plt.subplots()
+    ax.scatter([i[0] for i in points], [i[1] for i in points])
 
-    # plotting function
     lower_bound = min(points, key=lambda x: x[0])[0]
     upper_bound = max(points, key=lambda x: x[0])[0]
     x_test_points = np.linspace(lower_bound - 1, upper_bound + 1)
-    y_test_points = [f(i) for i in x_test_points] 
-    fig.plot(x_test_points, y_test_points)
+    y_test_points = [f(i) for i in x_test_points]
+    ax.plot(x_test_points, y_test_points)
     plt.legend()
     plt.show()
-    
-def interpolation_with_fx(): 
-    # reading the f(x)
+
+def interpolation_with_fx():
+    """
+    Interpolates using a given function f(x).
+    """
     f = input("f(x) = ")
     f = sympy.utilities.lambdify("x", str(f), "numpy")
-    
-    # Reading the interpolation points
+
     n = 0
     points = []
-    while(1):
+    while True:
         point = input(f"x{n}: ")
-        if point != "":
-            point = float(point)
-            points.append([point, f(point)])
-        else:
+        if not point:
             break
+        point = float(point)
+        points.append([point, f(point)])
         n += 1
 
-    # finding the interpolation funciton
     x = sympy.Symbol('x')
     interpolation_function = 0
     for i in range(n):
@@ -83,32 +85,29 @@ def interpolation_with_fx():
 
     interpolation_function = sympy.utilities.lambdify("x", str(interpolation_function), "numpy")
 
-    # plotting points
-    fig = plt.subplot()
-    fig.scatter([i[0] for i in points], [i[1] for i in points])
+    fig, ax = plt.subplots()
+    ax.scatter([i[0] for i in points], [i[1] for i in points])
 
-    # plotting function
     lower_bound = min(points, key=lambda x: x[0])[0]
     upper_bound = max(points, key=lambda x: x[0])[0]
     x_test_points = np.linspace(lower_bound - 1, upper_bound + 1)
-    y_test_points = [interpolation_function(i) for i in x_test_points] 
-    y_actual_points = [f(i) for i in x_test_points] 
-    fig.plot(x_test_points, y_test_points)
-    fig.plot(x_test_points, y_actual_points)
+    y_test_points = [interpolation_function(i) for i in x_test_points]
+    y_actual_points = [f(i) for i in x_test_points]
+    ax.plot(x_test_points, y_test_points)
+    ax.plot(x_test_points, y_actual_points)
     plt.show()
 
 def newton_main():
     print("1. Enter f(x) with points")
     print("2. Enter interpolation points in this format: (xi, f(xi))")
     print("=> ", end='')
-    
+
     command = input()
-    
+
     if command == "1":
         interpolation_with_fx()
     else:
         interpolation_with_points()
-
 
 if __name__ == '__main__':
     newton_main()
